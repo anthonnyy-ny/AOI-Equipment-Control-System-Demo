0784237a313477b27a01766e68dfeff26de9c981
Chuyanglife <yaohong96@gmail.com>
2026-05-30 22:29:35 +0800

    Create MachineState.cs

---
+++ AOIEquipmentControlSystem/Models/MachineState.cs
@@ -0,0 +1,14 @@
+﻿namespace AOIEquipmentControlSystem.Models
+{
+    public enum MachineState
+    {
+        Idle,
+        Initializing,
+        Ready,
+        Running,
+        Inspecting,
+        Completed,
+        Alarm,
+        Stopped
+    }
+}
